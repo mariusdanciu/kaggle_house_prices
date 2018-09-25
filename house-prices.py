4aefd12a8b817888ec60eb1405b8f5bca4d2c871
mariusdanciu <marius.danciu@ro.ibm.com>
2018-09-25 09:56:07 +0300

    minor updates

--- house-prices.py
+++ house-prices.py
@@ -12,13 +12,10 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Imputer
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeRegressor
 
 from MLE import MultiLabelEncoder
-
-imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
-norm = Normalizer('l2')
 
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 pd.set_option('display.max_rows', 1000)
@@ -31,16 +28,6 @@ testDf = pd.read_csv(testFile, header=0)
 
 trainDf['MoSold'] = trainDf['MoSold'].apply(str)
 testDf['MoSold'] = testDf['MoSold'].apply(str)
-
-# trainDf['OverallQual'] = trainDf['OverallQual'].apply(np.log)
-# #trainDf['GrLivArea'] = trainDf['GrLivArea'].pow(3) #nope
-# trainDf['GarageCars'] = trainDf['GarageCars'].pow(2)
-# trainDf['GarageArea'] = trainDf['GarageArea'].pow(2)
-#
-# testDf['OverallQual'] = testDf['OverallQual'].apply(np.log)
-# #testDf['GrLivArea'] = testDf['GrLivArea'].pow(3)
-# testDf['GarageCars'] = testDf['GarageCars'].pow(2)
-# testDf['GarageArea'] = testDf['GarageArea'].pow(2)
 
 target = 'SalePrice'
 
@@ -64,9 +51,11 @@ def prepare_data():
 
     print("Number of string columns %d " % len(str_cols))
 
+    # treat NaN as a different category
     for c in str_cols:
         training[c] = training[c].fillna("$NULL")
         testing[c] = testing[c].fillna("$NULL")
+
 
     print(training.dtypes)
     print(training.head(10))
@@ -75,7 +64,8 @@ def prepare_data():
 
     t_pipe = Pipeline(steps=[
         ('catencode', enc),
-        ('null_handler', imp)])
+        ('null_handler', Imputer(missing_values='NaN', strategy='mean', axis=0))
+    ])
 
     fit_pipeline = t_pipe.fit(pd.concat([training, testing], axis=0))
 
@@ -110,7 +100,7 @@ def correlations(t_df):
 def cv(df, pipeline):
     iter_rmsle = []
     iteration = 0
-    kf = KFold(n_splits=10, random_state=0)
+    kf = KFold(n_splits=10, random_state=10)
     for train_idx, test_idx in kf.split(df):
         print("KFold iteration ", iteration)
         x_train, x_test = df.iloc[train_idx], df.iloc[test_idx]
@@ -156,23 +146,25 @@ def select_pipeline(t_df, make_pipelines):
 
 def decision_tree_regressor():
     pipelines = []
-    est = DecisionTreeRegressor(criterion='mse', max_depth=7, max_features=None,
-                                max_leaf_nodes=None, min_impurity_decrease=0.0,
-                                min_impurity_split=None, min_samples_leaf=1,
-                                min_samples_split=2, min_weight_fraction_leaf=0.0,
-                                presort=False, random_state=0, splitter='best')
-    pipelines.append(Pipeline(steps=[('lr', est)]))
+    for split in range(2, 10):
+        est = DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None,
+                                    max_leaf_nodes=None, min_impurity_decrease=0.0,
+                                    min_impurity_split=None, min_samples_leaf=1,
+                                    min_samples_split=split, min_weight_fraction_leaf=0.0,
+                                    presort=False, random_state=10, splitter='best')
+        pipelines.append(Pipeline(steps=[('lr', est)]))
     return pipelines
 
 
 def xgb_regressor():
     pipelines = []
     for l in [0, 0.5, 0.7, 1.0, 1.5, 2]:
-        est = xgboost.XGBRegressor(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.7, gamma=0,
-                                   learning_rate=0.05, max_delta_step=0, max_depth=5,
-                                   min_child_weight=4, missing=None, n_estimators=500, nthread=-1,
-                                   objective='reg:linear', reg_alpha=0, reg_lambda=l,
-                                   scale_pos_weight=1, seed=0, silent=True, subsample=0.75)
+        for d in range(3, 10):
+            est = xgboost.XGBRegressor(base_score=0.5, colsample_bylevel=1, colsample_bytree=0.8, gamma=0,
+                                       learning_rate=0.1, max_delta_step=0, max_depth=d,
+                                       min_child_weight=10, missing=None, n_estimators=300, nthread=-1,
+                                       objective='reg:linear', reg_alpha=0.2, reg_lambda=l,
+                                       scale_pos_weight=1, seed=0, silent=True, subsample=0.8)
 
         pipelines.append(Pipeline(steps=[('lr', est)]))
 
@@ -183,7 +175,7 @@ def ridge():
     pipelines = []
     for l in [0, 0.5, 0.7, 1.0, 1.5, 2]:
         est = Ridge(alpha=l, copy_X=False, fit_intercept=True, max_iter=None,
-                    normalize=True, random_state=0, solver='auto', tol=0.001)
+                    normalize=True, random_state=10, solver='auto', tol=0.001)
         pipelines.append(Pipeline(steps=[('lr', est)]))
     return pipelines
 
@@ -192,16 +184,6 @@ def linear():
     pipelines = []
     est = LinearRegression(normalize=True)
     pipelines.append(Pipeline(steps=[('lr', est)]))
-    return pipelines
-
-
-def lasso():
-    pipelines = []
-    for l in [0, 0.5, 0.7, 1.0, 1.5, 2]:
-        est = Lasso(alpha=l, copy_X=True, fit_intercept=True, max_iter=1000,
-                    normalize=False, positive=False, precompute=False, random_state=0,
-                    selection='cyclic', tol=0.0001, warm_start=False)
-        pipelines.append(Pipeline(steps=[('lr', est)]))
     return pipelines
 
 
@@ -221,14 +203,13 @@ def stacking(training,
              y,
              test,
              pipelines):
-
     train, valid, y_train, y_valid = train_test_split(training, y, test_size=0.5)
 
     meta_validation = []
     meta_test = []
 
     for alg in pipelines:
-        #copy because algs with normalization will mutate the sata :(
+        # copy because algs with normalization will mutate the sata :(
         t_cpy = train.copy()
         alg_model = alg.fit(t_cpy, y_train)
 
@@ -253,22 +234,25 @@ def stacking(training,
 
 train_data, test_data = prepare_data()
 
-ridge_model, ridge_metrics = select_pipeline(train_data, ridge)
 
-xgb_model, xgb_metrics = select_pipeline(train_data, xgb_regressor)
-
+print("Run DT")
 dt_model, dt_metrics = select_pipeline(train_data, decision_tree_regressor)
 
+print("Run XGB")
+xgb_model, xgb_metrics = select_pipeline(train_data, xgb_regressor)
+
+
+print("Run Ridge")
+ridge_model, ridge_metrics = select_pipeline(train_data, ridge)
+
+print("Run LR")
 linear_model, lr_metrics = select_pipeline(train_data, linear)
 
-lasso_model, lasso_metrics = select_pipeline(train_data, lasso)
-
-labels = ['xgb', 'decision-tree', 'ridge', 'linear', "lasso"]
+labels = ['xgb', 'decision-tree', 'ridge', 'linear']
 mins = [np.min(xgb_metrics),
         np.min(dt_metrics),
         np.min(ridge_metrics),
-        np.min(lr_metrics),
-        np.min(lasso_metrics)]
+        np.min(lr_metrics)]
 
 index = np.arange(len(labels))
 
@@ -283,8 +267,8 @@ meta_model, stacked_test = stacking(
     train_data,
     Y,
     test_data,
-    [xgb_model, dt_model, ridge_model, linear_model, lasso_model])
+    [xgb_model, dt_model, ridge_model, linear_model])
 
 predict(meta_model, stacked_test)
 
-#predict(xgb_model, test_data)
+# predict(xgb_model, test_data)
